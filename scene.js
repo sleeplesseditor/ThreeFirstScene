c15ba0cc4f21c20f910326e11d4987ef8dca6226
Josh Bryson <josh@bryson.me.uk>
2020-03-01 00:06:26 +0000

    Added a rotating cube

--- scene.js
+++ scene.js
@@ -1,8 +1,17 @@
 // set up the environment - 
   // initiallize scene, camera, objects and renderer
-  let scene, camera, renderer;
+  let scene, camera, renderer, cube;
     // set up the environment - 
     // initiallize scene, camera, objects and renderer
+    let Add = 0.01;
+
+    let createCube = function(){
+      let geometry = new THREE.BoxGeometry(1,1,1);
+      let material = new THREE.MeshBasicMaterial({color: 0x00a1cb});
+      cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+    };
+
     let init = function() {
         // create the scene
         scene = new THREE.Scene();
@@ -14,19 +23,25 @@
                         window.innerWidth / window.innerHeight, 
                         1, 1000);
         camera.position.z = 5;
-        
+
+        createCube();
         
         // create the renderer   
         renderer = new THREE.WebGLRenderer();   
         renderer.setSize(window.innerWidth, window.innerHeight);
         
-        document.body.appendChild(renderer.domElement);
-        
+        document.body.appendChild(renderer.domElement);        
     };
   
     // main animation loop - calls 50-60 in a second.
     let mainLoop = function() {
-        console.log("Hello");
+        // cube.position.x += Add;
+        cube.position.x += Add;    
+         cube.rotation.z -= Add;
+        
+        if(cube.position.x <= -3 || cube.position.x >= 3)
+          Add *= -1;
+
         renderer.render(scene, camera);
         requestAnimationFrame(mainLoop);
     };
